956b6db105fce72a59f3ae2d975d8245504e30ca
NadiaAiraf <aidanfaria@gmail.com>
2018-11-04 17:22:42 +0000

    test a user can log in and out once signed up

--- spec/features/test_adding_user_spec.rb
+++ spec/features/test_adding_user_spec.rb
@@ -3,4 +3,14 @@ RSpec.describe 'Posts page' do
     sign_up_1
     expect(page).to have_content("Logged in as test@gmail.com")
   end
+
+  it "Once signed up, a user can log out then log in again" do
+    sign_up_1
+    click_link 'Logout'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+    fill_in 'user[email]', with: "test@gmail.com"
+    fill_in 'user[password]', with: "password"
+    click_button 'Log in'
+    expect(page).to have_content("Logged in as test@gmail.com")
+  end
 end
